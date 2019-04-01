45e96c765ba14f17a7c387f85d9922e39f07f489
Andy Bettisworth <andy@accreu.com>
2019-04-01 16:10:04 -0500

    complete arrow function conversions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,23 +3,42 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();mm
+// myFunction();
+
+const myFunction = () => {
+  console.log("Function was invoked!")
+}
+myFunction()
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+const anotherFunction = (param) => {
+  return param
+}
+console.log(anotherFunction("Example"))
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+const add = (param1, param2) => {
+  return param1 + param2
+}
+console.log(add(1,2))
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (param1, param2) => {
+  return param1 - param2
+}
+console.log(subtract(1,2))
 
 // Stretch
 
@@ -28,3 +47,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+const exampleArray = [1,2,3,4]
+const triple = exampleArray.map(num => num * 3)
+console.log(triple)
